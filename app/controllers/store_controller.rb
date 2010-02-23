b5535ace180d9e8fb754d78cd35bd1cfbec1792b
Roman Sklenar <mail@romansklenar.cz>
2010-02-23 22:45:16 +0100

    Created action and view checkout in StoreController

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -9,8 +9,8 @@ class StoreController < ApplicationController
     begin
       product = Product.find(params[:id])
     rescue ActiveRecord::RecordNotFound
-      logger.error("Attempt to access invalid product #{params[:id]}" )
-      redirect_to_index("Invalid product" )
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      redirect_to_index("Invalid product")
     else
       @cart = find_cart
       @current_item = @cart.add_product(product)
@@ -26,6 +26,16 @@ class StoreController < ApplicationController
   def empty_cart
     session[:cart] = nil
     redirect_to_index
+  end
+
+
+  def checkout
+    @cart = find_cart
+    if @cart.items.empty?
+      redirect_to_index("Your cart is empty")
+    else
+      @order = Order.new
+    end
   end
 
 

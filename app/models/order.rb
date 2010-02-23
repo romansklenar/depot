f60a0ade86c86b2fe1dfe0f63a5b81074e6b5799
Roman Sklenar <mail@romansklenar.cz>
2010-02-23 22:50:07 +0100

    Created form in view checkout

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ActiveRecord::Base
   has_many :line_items
+
+  PAYMENT_TYPES = [
+    #  Displayed       stored in db
+    [ "Check",          "check" ],
+    [ "Credit card",    "cc" ],
+    [ "Purchase order", "po" ]
+  ]
+
 end

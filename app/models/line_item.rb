bf8543f162092dafd60faafffff08a2542458c50
Roman Sklenar <mail@romansklenar.cz>
2010-02-23 23:12:42 +0100

    LineItem model has implemented factory from CartItem

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,12 @@
 class LineItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
+
+  def self.from_cart_item(cart_item)
+    line_item = self.new
+    line_item.product = cart_item.product
+    line_item.quantity = cart_item.quantity
+    line_item.total_price = cart_item.price
+    line_item
+  end
 end

47fe7f252e6c903419a4baf03f4b932f74dd627d
Roman Sklenar <mail@romansklenar.cz>
2010-03-09 18:18:10 +0100

    Functional test for Admin controller

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -1,8 +1,64 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :users
+
+
+  def setup
+    @controller = AdminController.new
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
   end
+
+
+  def test_index_without_user
+    get :index
+    assert_redirected_to :controller => :admin, :action => :login
+    assert_equal "Please log in", flash[:notice]
+  end
+
+
+  def test_index_with_user
+    get :index, {}, { :user_id => users(:dave).id }
+    assert_response :success
+    assert_template :index
+  end
+
+
+  def test_login
+    dave = users(:dave)
+    post :login, :name => dave.name, :password => 'secret'
+
+    assert_redirected_to :controller => :admin, :action => :index
+    assert_equal dave.id, session[:user_id]
+  end
+
+
+  def test_bad_password
+    dave = users(:dave)
+    post :login, :name => dave.name, :password => 'wrong'
+
+    assert !session[:user_id]
+    assert_template :login
+  end
+
+
+  def test_user_is_redirected_once_authenticated
+    @request.session[:original_uri] = users_path
+    dave = users(:dave)
+    post :login, :name => dave.name, :password => 'secret'
+
+    assert_redirected_to users_path
+  end
+
+
+  def test_user_gets_logged_out
+    @request.session[:user_id] = users(:dave).id
+    get :logout
+
+    assert_redirected_to :controller => :admin, :action => :login
+    assert !@response.session[:user_id]
+    assert_equal @response.flash[:notice], "Logged out"
+  end
+
 end
